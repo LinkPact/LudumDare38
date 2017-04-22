abdd4ecbdf2ae4b73658c31a27ef03e957f074a3
Johan37 <johan.philipsson@hotmail.com>
2017-04-22 19:40:45 +0200

    Create wood object

---
+++ Assets/scripts/WorldObjects/PickableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour {
+
+    private StoryController story_controller;
+    public string message = "template message";
+    private Inventory inventory;
+    public Item item_to_spawn;
+
+    void Start()
+    {
+        story_controller = GameObject.FindObjectOfType<StoryController>();
+        inventory = GameObject.FindObjectOfType<Inventory>();
+    }
+
+    void OnMouseDown()
+    {
+        story_controller.ShowText(message);
+        inventory.AddItem(Instantiate(item_to_spawn));
+        Destroy(gameObject);
+    }
+}
