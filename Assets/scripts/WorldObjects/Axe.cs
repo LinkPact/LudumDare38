6f8d9fadaeb6f1f46b7a66e69c2ed8d93c6801c5
jakob <jakob.willforss@hotmail.com>
2017-04-22 13:55:02 +0200

    Spawning berry bush. Ax object. Add inventory sprites.

---
+++ Assets/scripts/WorldObjects/Axe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour {
+
+    private StoryController story_controller;
+    public string message = "template message";
+
+    void Start () {
+        story_controller = GameObject.FindObjectOfType<StoryController>();
+    }
+
+    void OnMouseDown() {
+        story_controller.ShowText(message);
+        Destroy(gameObject);
+    }
+}
--- Assets/scripts/WorldObjects/BerryBush.cs
+++ Assets/scripts/WorldObjects/BerryBush.cs
@@ -6,8 +6,11 @@ public class BerryBush : MonoBehaviour {
 
     public Sprite[] sprites;
 
-    public bool has_berries = true;
+    public bool has_berries;
     public float food_value;
+
+    public float berry_spawn_time;
+    private float current_spawn_time;
 
     private SpriteRenderer sprite_renderer;
     private NeedsManager needs;
@@ -15,20 +18,37 @@ public class BerryBush : MonoBehaviour {
 	void Start () {
         sprite_renderer = GetComponent<SpriteRenderer>();
         needs = GameObject.FindObjectOfType<NeedsManager>();
+        has_berries = true;
 	}
 	
 	void Update () {
-		
+
+        UpdateSpawn();
+        AssignSprite();
+	}
+
+    private void UpdateSpawn() {
+        current_spawn_time += Time.deltaTime;
+        if (current_spawn_time > berry_spawn_time) {
+            has_berries = true;
+            current_spawn_time = 0;
+        }
+    }
+
+    private void AssignSprite() {
         if (has_berries) {
             sprite_renderer.sprite = sprites[0];
         }
         else {
             sprite_renderer.sprite = sprites[1];
         }
-	}
+    }
 
     void OnMouseDown() {
-        has_berries = !has_berries;
-        needs.ReduceHunger(food_value);
+
+        if (has_berries) {
+            has_berries = false;
+            needs.ReduceHunger(food_value);
+        }
     }
 }
