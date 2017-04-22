d167a7827cb1d3886f42723fac10fdc8535fbc3e
jakob <jakob.willforss@hotmail.com>
2017-04-22 09:57:36 +0200

    Collision against sea and camera panning

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    private Transform player_trans;
+
+    private Vector3 orig_pos;
+
+	void Start () {
+        player_trans = GameObject.FindObjectOfType<Player>().gameObject.transform;
+        orig_pos = transform.position;
+	}
+	
+	void Update () {
+
+        
+
+        transform.position = new Vector3(player_trans.position.x, player_trans.position.y, orig_pos.z);
+	}
+}
