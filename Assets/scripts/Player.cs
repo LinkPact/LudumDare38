3eee9c98127ed6da4c461af9f3d6f8315438f599
jakob <jakob.willforss@hotmail.com>
2017-04-22 09:33:14 +0200

    Player movement. Texture fixes.

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float move_speed = 0.01f;
+
+    private SpriteRenderer renderer;
+    private Rigidbody2D rigi;
+
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        rigi = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+        UpdateMovement();
+
+	}
+
+    private void UpdateMovement() {
+
+        float horiz_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+
+        if (horiz_input != 0) {
+            transform.position = new Vector2(transform.position.x + move_speed * horiz_input, transform.position.y);
+        }
+
+        if (vertical_input != 0) {
+            transform.position = new Vector2(transform.position.x, transform.position.y + move_speed * vertical_input);
+        }
+    }
+}
