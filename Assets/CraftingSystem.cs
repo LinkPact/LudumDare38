d10c2f2b30cd980c23032db62513ff98df227ef3
jakob <jakob.willforss@hotmail.com>
2017-04-23 08:28:43 +0200

    Toggle crafting system

---
+++ Assets/CraftingSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingSystem : MonoBehaviour {
+
+    private CraftMenuToggleButton toggle_button;
+    private GameObject crafting_options_menu_object;
+
+    private bool is_craft_menu_currently_active;
+
+	void Start () {
+        toggle_button = GetComponent<CraftMenuToggleButton>();
+        crafting_options_menu_object = GetComponentInChildren<CraftingOptions>().gameObject;
+
+        is_craft_menu_currently_active = false;
+	}
+	
+	void Update () {
+		
+	}
+
+    public void ToggleCraftMenuOptions() {
+        is_craft_menu_currently_active = !is_craft_menu_currently_active;
+        crafting_options_menu_object.SetActive(is_craft_menu_currently_active);
+    }
+}
