96a17460a460a183cfac3f9a6d14d4f7e9cb8aea
jakob <jakob.willforss@hotmail.com>
2017-04-24 19:09:23 +0200

    Exit on escape and limited run ratio

--- Assets/GameEventSystem.cs
+++ Assets/GameEventSystem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventSystem : MonoBehaviour {
+public class GameEventSystem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +12,9 @@ public class EventSystem : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+        if (Input.GetKey("escape")) {
+            Application.Quit();
+        }
+
 	}
 }
